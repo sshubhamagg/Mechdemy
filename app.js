e3a4bd0e17744432b433432366ace30951d43b51
shubham <aggshubh95@gmail.com>
2020-01-26 22:07:21 +0530

    updated .env

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express=require('express');
-var mongoose=require('mongoose');
-var cors=require('cors');
-var bodyParser=require('body-parser')
+var express = require('express');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var bodyParser = require('body-parser')
 
-var app=express();
-
+var app = express();
+require('dotenv').config();
 
 const routes = require('./router/indexRoute');
-const port=3000;
-const dbUrl="mongodb://127.0.0.1:27017/mechademy";
+const port = process.env.port;
+const dbUrl = process.env.dbUrl;
 
 mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
 
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
     console.log('mongodb connected');
-    
+
 })
 
-mongoose.connection.on('error',(error)=>{
-    console.log('mongodb connection failed',error);
-    
+mongoose.connection.on('error', (error) => {
+    console.log('mongodb connection failed', error);
+
 })
 
 
@@ -27,14 +27,14 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, PUT,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization,Accept-Language');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, PUT,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization,Accept-Language');
+    next();
 });
 
-app.use('/mechademy',routes)
+app.use('/mechademy', routes)
 
-app.listen(port,()=>{
-    console.log('server listening to port',port);    
+app.listen(port, () => {
+    console.log('server listening to port', port);
 })
--- services/userService.js
+++ services/userService.js
@@ -1,49 +1,43 @@
 var userModel = require('../models/userModel');
 var index = require('../utils/index');
 var bcrypt = require('bcrypt');
-var _=require('lodash');
+var _ = require('lodash');
 
 module.exports.signup = async (data) => {
-    console.log('inside signup');
-    
-    var user=await this.findUser(data.userName);
-    if(_.isNull(user.data)){
-    console.log('saving signup details');
-    return new Promise(async (resolve, reject) => {
-        let userObject = new userModel(data);
-        var pass = await index.hashPassword(data.password);
-        userObject.password = pass;
-        userObject.save((error, response) => {
-            if (error) {
-                console.log(error);
-                reject(error);
-            }
-            else {
-                resolve({ success: true, data: response });
-            }
-        })
-    }).catch((error) => {
-        return { success: false, error: error }
-    });
-}
-else{
-    return({ success: false, error: 'Username already exists' })
-}
+
+    var user = await this.findUser(data.userName);
+    if (_.isNull(user.data)) {
+        return new Promise(async (resolve, reject) => {
+            let userObject = new userModel(data);
+            var pass = await index.hashPassword(data.password);
+            userObject.password = pass;
+            userObject.save((error, response) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve({ success: true, data: response });
+                }
+            })
+        }).catch((error) => {
+            return { success: false, error: error }
+        });
+    }
+    else {
+        return ({ success: false, error: 'Username already exists' })
+    }
 };
 
 
-module.exports.login = async (data) => {    
+module.exports.login = async (data) => {
     return new Promise(async (resolve, reject) => {
-        console.log('data', data);
         userModel.findOne({ userName: data.userName }, (error, response) => {
             if (error) {
-                console.log(error);
                 reject(error);
             }
             else {
                 if (_.isNull(response)) {
-                    console.log('user not found');
-                    return reject ({ success: false, error: 'user not found' })
+                    return reject({ success: false, error: 'user not found' })
                 }
                 else {
                     bcrypt.compare(data.password, response.password, function (err, result) {
@@ -64,20 +58,17 @@ module.exports.login = async (data) => {
 }
 
 
-module.exports.findUser=async(userName)=>{
-    console.log('inside find user');
-    
+module.exports.findUser = async (userName) => {
     return new Promise(async (resolve, reject) => {
         userModel.findOne({ userName: userName }, (error, response) => {
             if (error) {
-                console.log(error);
                 reject(error);
             }
             else {
-                resolve({success:true,data:response})
+                resolve({ success: true, data: response })
             }
         })
-    }).catch((error)=>{
-        return({success:false,error:error})
+    }).catch((error) => {
+        return ({ success: false, error: error })
     })
 }
